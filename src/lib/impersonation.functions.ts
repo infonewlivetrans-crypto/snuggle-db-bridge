8069495c16d9d6f99e46dd85988a559d917bbbdc
Stanislav <info.newlive.trans@gmail.com>
2026-05-11 21:21:42 +0000

    Working deploy with SSL and dev server

--- src/lib/impersonation.functions.ts
+++ src/lib/impersonation.functions.ts
@@ -1,5 +1,7 @@
-// Клиент-безопасный реэкспорт серверных функций имперсонации.
-export {
-  startImpersonationFn,
-  stopImpersonationFn,
-} from "@/server/impersonation.functions";
+export async function startImpersonationFn(..._args: unknown[]) {
+  throw new Error("Impersonation is disabled in production build");
+}
+
+export async function stopImpersonationFn(..._args: unknown[]) {
+  throw new Error("Impersonation is disabled in production build");
+}
